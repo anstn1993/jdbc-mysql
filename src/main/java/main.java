0b209e8a026f7f3770f3ef0e3fb64c83495daf79
moonsoo <anstn1993@gmail.com>
2019-12-21 21:55:16 +0900

    added close connection

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,18 +3,20 @@ import java.sql.*;
 public class main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        String url = "jdbc:mysql://<ip_address>:3306(default port number of mysql)/<name of database>";
+        String url = "jdbc:mysql://13.124.105.47:3306/SNS";
         String sql = "SELECT*FROM user";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, "user", "password");
+        Connection con = DriverManager.getConnection(url, "moonsoo", "Rla933466r!");
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(sql);
 
         while(rs.next()) {
             System.out.println(rs.getString("name"));
-
         }
 
+        rs.close();
+        st.close();
+        con.close();
     }
 }
